7de7f1701f15bac89ce1434b491a8f2b942bcf15
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-08 16:39:26 -0400

    Moved the sample data over from the view and made it an extension on the DataPoint struct

--- SwiftUIChartsPractice/ContentView.swift
+++ SwiftUIChartsPractice/ContentView.swift
@@ -9,40 +9,16 @@ import SwiftUI
 import Charts
 
 struct ContentView: View {
-    
-    var data = [
-        DataPoint(day: "Sun", hours: 8, type: "Work"),
-        DataPoint(day: "Mon", hours: 8, type: "Work"),
-        DataPoint(day: "Tues", hours: 8, type: "Work"),
-        DataPoint(day: "Wed", hours: 8, type: "Work"),
-        DataPoint(day: "Thurs", hours: 8, type: "Work"),
-        DataPoint(day: "Fri", hours: 8, type: "Work"),
-        DataPoint(day: "Sat", hours: 8, type: "Work"),
-        // - Data points below this point are stacked bars
-        DataPoint(day: "Sun", hours: 3, type: "Nap"),
-        DataPoint(day: "Sun", hours: 1, type: "Gym"),
-        DataPoint(day: "Mon", hours: 2, type: "AppleTV+"),
-        DataPoint(day: "Tues", hours: 2, type: "Nap"),
-        DataPoint(day: "Tues", hours: 1, type: "Gym"),
-        DataPoint(day: "Wed", hours: 2, type: "Nap"),
-        DataPoint(day: "Thurs", hours: 2, type: "AppleTV+"),
-        DataPoint(day: "Thurs", hours: 1, type: "Gym"),
-        DataPoint(day: "Fri", hours: 4, type: "Overwatch"),
-        DataPoint(day: "Sat", hours: 2, type: "Nap"),
-        DataPoint(day: "Sat", hours: 1, type: "Gym")
-    ]
+    let sampleData = DataPoint.sampleData
     
     var body: some View {
-        
         VStack {
             Text("A Basic SWiftUI Bar Chart")
                 .font(.title3)
                 .bold()
             
             Chart {
-                
-                ForEach(data) { d in
-                    
+                ForEach(sampleData) { d in
                     BarMark(x: .value("Day", d.day), y: .value("Hours", d.hours))
                         .annotation (position: .overlay) {
                             Text(String(d.hours)+"h")
--- SwiftUIChartsPractice/DataPoint.swift
+++ SwiftUIChartsPractice/DataPoint.swift
@@ -14,3 +14,28 @@ struct DataPoint: Identifiable {
     var hours: Int
     var type: String
 }
+
+extension DataPoint {
+    static let sampleData: [DataPoint] =
+    [
+        DataPoint(day: "Sun", hours: 8, type: "Work"),
+        DataPoint(day: "Mon", hours: 8, type: "Work"),
+        DataPoint(day: "Tues", hours: 8, type: "Work"),
+        DataPoint(day: "Wed", hours: 8, type: "Work"),
+        DataPoint(day: "Thurs", hours: 8, type: "Work"),
+        DataPoint(day: "Fri", hours: 8, type: "Work"),
+        DataPoint(day: "Sat", hours: 8, type: "Work"),
+        // - Data points below this point are stacked bars
+        DataPoint(day: "Sun", hours: 3, type: "Nap"),
+        DataPoint(day: "Sun", hours: 1, type: "Gym"),
+        DataPoint(day: "Mon", hours: 2, type: "AppleTV+"),
+        DataPoint(day: "Tues", hours: 2, type: "Nap"),
+        DataPoint(day: "Tues", hours: 1, type: "Gym"),
+        DataPoint(day: "Wed", hours: 2, type: "Nap"),
+        DataPoint(day: "Thurs", hours: 2, type: "AppleTV+"),
+        DataPoint(day: "Thurs", hours: 1, type: "Gym"),
+        DataPoint(day: "Fri", hours: 4, type: "Overwatch"),
+        DataPoint(day: "Sat", hours: 2, type: "Nap"),
+        DataPoint(day: "Sat", hours: 1, type: "Gym")
+    ]
+}
