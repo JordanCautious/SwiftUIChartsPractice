cf7154b1b422feca57c29acf03b79d12d15a5e7d
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-08 02:45:35 -0400

    Initial Commit

---
+++ SwiftUIChartsPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIChartsPractice
+//
+//  Created by Jordan Haynes on 10/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIChartsPractice/SwiftUIChartsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsPracticeApp.swift
+//  SwiftUIChartsPractice
+//
+//  Created by Jordan Haynes on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
